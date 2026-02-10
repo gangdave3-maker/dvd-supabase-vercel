ce52a1e93c326827101164dc7ee9970596ae6381
gangdave <60885732+gangdave@users.noreply.github.com>
2026-02-10 22:48:22 +0700

    fix build

--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -1,14 +1,72 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 import Link from 'next/link'
 import { delCookies } from '../api/del-cookies/route'
+import { Menu, X } from 'lucide-react'
 
 function Nav() {
+  const [open, setOpen] = useState(false)
+
+  const handleLogout = () => {
+    delCookies()
+    setOpen(false)
+  }
+
   return (
-    <div className='bgc flex justify-center items-center gap-x-40'>
-      <Link href={'/Hall'} className='navfont glow'>DVD</Link>
-      <Link href={'/Contact'} className='navfont glow'>About Us</Link>
-      <Link href={'/'} className='navfont glow' onClick={()=>delCookies()}>Logout</Link>
-    </div>
+    <nav className="bgc">
+      <div className="container flex items-center! align-middle! justify-center px-4">
+        <div className="hidden md:flex items-center! align-middle! gap-x-40 mt-2!">
+          <Link href={'/Hall'} className='navfont glow'>DVD</Link>
+          <Link href={'/Contact'} className='navfont glow'>About Us</Link>
+          <Link href={'/'} className='navfont glow' onClick={()=>handleLogout()}>Logout</Link>
+        </div>
+      </div>
+
+      <div className="container flex items-start! mt-3! justify-end">
+        {/* Mobile Menu */}
+        {open && (
+          <div className="md:hidden bgc border-t border-white/10">
+            <div className="flex flex-col items-center gap-6 py-6">
+
+              <Link
+                href="/Hall"
+                className="navfont glow"
+                onClick={() => setOpen(false)}
+              >
+                DVD
+              </Link>
+
+              <Link
+                href="/Contact"
+                className="navfont glow"
+                onClick={() => setOpen(false)}
+              >
+                About Us
+              </Link>
+
+              <Link
+                href="/"
+                className="navfont glow"
+                onClick={handleLogout}
+              >
+                Logout
+              </Link>
+
+            </div>
+          </div>
+        )}
+        {/* Hamburger Button (Mobile) */}
+          <button
+            className="md:hidden text-white"
+            onClick={() => setOpen(!open)}
+            aria-label="Toggle menu"
+          >
+            {open ? <X size={32} /> : <Menu size={32} />}
+          </button>
+      </div>
+        
+    </nav>
+    
   )
 }
 
